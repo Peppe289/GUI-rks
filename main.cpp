80c4774cde9516378e5d08b6be2095329097c6ed
Peppe289 <gsperanza204@gmail.com>
2022-04-28 21:06:35 +0200

    initial cmd [show info]
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,18 +3,26 @@
 #include "include/tool.hpp"
 #include "include/class.hpp"
 
+static void _showinfo(info cpu) {
+    for (int i = 0; i != (int)cpu.cluster.size(); ++i)
+        std::cout<<cpu.cluster[i]<<"\n";
+
+    std::cout<<"Number of cluster : "<<cpu.cluster.size()<<"\n";
+}
+
 int main (int argc, char *argv[]) {
     info cpu;
     cpu.SearchCluster();
 
-/*  for (int i = 0; i != (int)cpu.cluster.size(); ++i)
-        std::cout<<cpu.cluster[i]<<"\n";
-
-    std::cout<<"Number of cluster : "<<cpu.cluster.size()<<"\n"; */
-
     cpu.maxfreq = _cpu_maxfreq(cpu.cluster);
 
-//  std::cout<<"Max CPU freq : "<<cpu.maxfreq<<" Ghz";
+    if (argc == 2) {
+        std::string cmd(argv[1]);
+
+        if (cmd == "--info") {
+            _showinfo(cpu);
+        }
+    }
 
     std::cout<<"\n";
     return 0;
