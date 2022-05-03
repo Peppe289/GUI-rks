cdac5f3b35d38f9a284173ff8e4d510598a3299f
Peppe289 <gsperanza204@gmail.com>
2022-05-03 20:37:54 +0200

    update print msg
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 #include "include/class.hpp"
 
 static void _showinfo(info cpu) {
-    for (int i = 0; i != (int)cpu.cluster.size(); ++i)
-        std::cout<<cpu.cluster[i]<<"\n";
+/*    for (int i = 0; i != (int)cpu.cluster.size(); ++i)
+        std::cout<<cpu.cluster[i]<<"\n";*/
 
     std::cout<<"Number of cluster : "<<cpu.cluster.size()<<"\n";
+
+    std::cout<<"Max freq : "<<cpu.maxfreq<<" Ghz\n";
 }
 
 int main (int argc, char *argv[]) {
