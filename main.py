2cd6b654241562afca2c2cdc1698df562b87c372
Peppe289 <gsperanza204@gmail.com>
2023-06-27 08:38:23 +0200

    update
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 import sys
 import ctypes
-import threading
 import time
+import psutil
 
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QMessageBox, QVBoxLayout, QGroupBox
 from PyQt6.QtGui import QPalette, QColor, QFont
 from PyQt6.QtCore import Qt, QThread, pyqtSignal
+
+class get_online_cpu_thread(QThread):
+    update_label_signal = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        #cpu_usage = libRKM.cpu_load
+        #cpu_usage.restype = ctypes.c_int
+        while(1):
+            #text = cpu_usage()
+            text = psutil.cpu_percent()
+            # print(text)
+            text = "CPU Usage: " + str(text) + "%"
+            # Emit the update_label_signal with the new label text
+            self.update_label_signal.emit(text)
+            time.sleep(1)
+
+class get_cpu_thread(QThread):
+    update_label_signal = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        cpu_online = libRKM.online_cpu
+        cpu_online.restype = ctypes.c_int
+        while(1):
+            text = cpu_online()
+            # print(text)
+            text = "CPU Online: " + str(text)
+            # Emit the update_label_signal with the new label text
+            self.update_label_signal.emit(text)
+            time.sleep(1)
 
 def load_libRKM():
     global libRKM
@@ -105,18 +140,21 @@ def main():
     # Create a font for the label
     font = QFont("Arial", 10, QFont.Weight.Bold)
     label.setFont(font)
-
-    # Create a group box
-    group_box = QGroupBox("", window)
-    group_box.setGeometry(0, 0, window_width, window_height - label_height - 3)
-
-    # Create a layout for the group box
-    layout = QVBoxLayout()
-    group_box.setLayout(layout)
-
     # Set text alignment
     label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
     label.setStyleSheet("background-color: black; color: white; border-radius; border-radius: 25px; margin: auto")
+
+    # Create a group box
+    group_box_left = QGroupBox("", window)
+    group_box_right = QGroupBox("", window)
+    group_box_left.setGeometry(0, 0, int(window_width / 2), window_height - label_height - 3)
+    group_box_right.setGeometry(int(window_width / 2), 0, int(window_width / 2), window_height - label_height - 3)
+
+    # Create a layout for the group box
+    layout_left = QVBoxLayout()
+    layout_right = QVBoxLayout()
+    group_box_left.setLayout(layout_left)
+    group_box_right.setLayout(layout_right)
 
     btn_ram = QPushButton("Clear RAM")
     #button2 = QPushButton("Nice")
@@ -125,7 +163,23 @@ def main():
 
     btn_ram.clicked.connect(clear_ram)
 
-    layout.addWidget(btn_ram)
+    layout_left.addWidget(btn_ram)
+
+    # show cpu usage
+    cpu_label = QLabel()
+    cpu_label.setMaximumSize(int(window_width / 2) - 1, 30)
+    layout_right.addWidget(cpu_label)
+    cpu_thread = get_cpu_thread()
+    cpu_thread.update_label_signal.connect(lambda new_text: cpu_label.setText(new_text))
+    cpu_thread.start()
+
+    # show CPU online
+    online_cpu = QLabel()
+    online_cpu.setMaximumSize(int(window_width / 2) - 1, 30)
+    layout_right.addWidget(online_cpu)
+    online_cpu_thread = get_online_cpu_thread()
+    online_cpu_thread.update_label_signal.connect(lambda new_text: online_cpu.setText(new_text))
+    online_cpu_thread.start()
 
     window.show()
     sys.exit(app.exec())
--- src/libRKM.c
+++ src/libRKM.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 #include "utils.h"
 
@@ -15,45 +16,6 @@ int clear_ram(void)
         return ERROR_POPEN;
 
     return pclose(cmd);
-}
-
-int cpu_load(void)
-{
-    unsigned long long prev_total_time = 0;
-    unsigned long long prev_idle_time = 0;
-
-    FILE *fp = fopen("/proc/stat", "r");
-    if (fp == NULL)
-    {
-        perror("Error opening /proc/stat");
-        return 1;
-    }
-
-    char buffer[BUFFER_SIZE];
-    fgets(buffer, BUFFER_SIZE, fp);
-    fclose(fp);
-
-    // Parse CPU usage information from the first line
-    char *token = strtok(buffer, " ");
-    unsigned long long user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice;
-    sscanf(token, "%*s %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal, &guest, &guest_nice);
-
-    unsigned long long total_time = user + nice + system + idle + iowait + irq + softirq + steal;
-    unsigned long long idle_time = idle + iowait;
-
-    unsigned long long total_time_diff = total_time - prev_total_time;
-    unsigned long long idle_time_diff = idle_time - prev_idle_time;
-
-    double cpu_usage = 100.0 * (total_time_diff - idle_time_diff) / total_time_diff;
-
-    printf("CPU Usage: %.2f%%\n", cpu_usage);
-
-    prev_total_time = total_time;
-    prev_idle_time = idle_time;
-
-    sleep(1); // Sleep for 1 second before reading CPU usage again
-
-    return 0;
 }
 
 int online_cpu(void)
