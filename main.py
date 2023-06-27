b9264792a3a953cc627cec31396d975d97a1a670
Peppe289 <gsperanza204@gmail.com>
2023-06-27 10:15:26 +0200

    update
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- main.py
+++ main.py
@@ -3,9 +3,46 @@ import ctypes
 import time
 import psutil
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QMessageBox, QVBoxLayout, QGroupBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QMessageBox, QVBoxLayout, QGroupBox, QComboBox, QProgressBar
 from PyQt6.QtGui import QPalette, QColor, QFont
 from PyQt6.QtCore import Qt, QThread, pyqtSignal
+
+class get_ram_usage(QThread):
+    update_label_signal = pyqtSignal(list)
+
+    def __init__(self):
+        super().__init__()
+    
+    def run(self):
+        while 1:
+            text = [' ', 0]
+            text[0] = "Ram usage: " + str(psutil.virtual_memory().percent) + "%"
+            text[1] = int(psutil.virtual_memory().percent)
+            self.update_label_signal.emit(text)
+            time.sleep(1)
+
+
+class set_current_gov_thread(QThread):
+    update_label_signal = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+    
+    def run(self):
+        while 1:
+            try:
+                with open("/sys/devices/system/cpu/cpufreq/policy0/scaling_governor") as f:
+                    text = f.readlines()[0].strip().split(" ")
+                #file = open("/sys/devices/system/cpu/cpufreq/policy0/scaling_governor")
+                #text = file.read()
+                #file.close()
+            except:
+                print_on_label("Some error to read current gov\n")
+                text = ['error']
+                break
+
+            self.update_label_signal.emit(text[0])
+            time.sleep(1)
 
 class get_online_cpu_thread(QThread):
     update_label_signal = pyqtSignal(str)
@@ -73,13 +110,17 @@ def show_popup():
     message_box.setStandardButtons(QMessageBox.StandardButton.Ok)
     message_box.exec()
 
-def clear_label():
+def print_on_label(data):
     global counter
     counter = counter + 1
 
     if counter > 10:
         label.setText("")
         counter = 1
+    
+    text = label.text()
+    text = text + data
+    label.setText(text)
 
 def clear_ram():
     clear_ram = libRKM.clear_ram
@@ -87,18 +128,24 @@ def clear_ram():
     clear_ram.restype = ctypes.c_int
     result = clear_ram()
 
-    clear_label()
-
-    text = label.text()
-
     if result != 0:
-        text = text + "Clear RAM: error to clear ram\n"
+        #text = text + "Clear RAM: error to clear ram\n"
+        print_on_label("Clear RAM: error to clear ram\n")
         show_popup()
     else:
-        text = text + "Clear RAM: done\n"
+        print_on_label("Clear RAM: done\n")
+        #text = text + "Clear RAM: done\n"
 
-    text = text
-    label.setText(text)
+def change_governor(data):
+    #cpu_governor.setCurrentText(data)
+    #print(data)
+    try:
+        file = open("/sys/devices/system/cpu/cpufreq/policy0/scaling_governor", "w")
+        file.write(data)
+        file.close()
+        print_on_label("Changed governor to " + data + "\n")
+    except:
+        print_on_label("You can't change governor\n")
 
 def main():
     # Create the application
@@ -149,6 +196,7 @@ def main():
     group_box_right = QGroupBox("", window)
     group_box_left.setGeometry(0, 0, int(window_width / 2), window_height - label_height - 3)
     group_box_right.setGeometry(int(window_width / 2), 0, int(window_width / 2), window_height - label_height - 3)
+    group_size = group_box_left.size()
 
     # Create a layout for the group box
     layout_left = QVBoxLayout()
@@ -156,18 +204,17 @@ def main():
     group_box_left.setLayout(layout_left)
     group_box_right.setLayout(layout_right)
 
+    layout_left.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+    layout_right.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+
     btn_ram = QPushButton("Clear RAM")
-    #button2 = QPushButton("Nice")
-    
-    btn_ram.setMaximumSize(100, 30)
-
+    btn_ram.setMinimumSize(int(group_size.width() / 2) - 2, 30)
     btn_ram.clicked.connect(clear_ram)
-
     layout_left.addWidget(btn_ram)
 
     # show cpu usage
     cpu_label = QLabel()
-    cpu_label.setMaximumSize(int(window_width / 2) - 1, 30)
+    cpu_label.setMinimumSize(int(group_size.width() / 2) - 2, 30)
     layout_right.addWidget(cpu_label)
     cpu_thread = get_cpu_thread()
     cpu_thread.update_label_signal.connect(lambda new_text: cpu_label.setText(new_text))
@@ -175,11 +222,46 @@ def main():
 
     # show CPU online
     online_cpu = QLabel()
-    online_cpu.setMaximumSize(int(window_width / 2) - 1, 30)
+    online_cpu.setMinimumSize(int(group_size.width() / 2) - 2, 30)
     layout_right.addWidget(online_cpu)
     online_cpu_thread = get_online_cpu_thread()
     online_cpu_thread.update_label_signal.connect(lambda new_text: online_cpu.setText(new_text))
     online_cpu_thread.start()
+
+    # show ram usage
+    ram_usage = QLabel()
+
+    ram_usage.setMinimumSize(int(group_size.width() / 2) - 2, 30)
+    layout_right.addWidget(ram_usage)
+
+    ram_usage_thread = get_ram_usage()
+    ram_usage_thread.update_label_signal.connect(lambda new_text: ram_usage.setText(new_text[0]))
+
+    ram_usage_bar = QProgressBar()
+    ram_usage_bar.setMaximumSize(group_size.width() - 2, 15)
+    ram_usage_bar.setMinimum(0)
+    ram_usage_bar.setMaximum(100)
+    layout_right.addWidget(ram_usage_bar)
+
+    ram_usage_thread.update_label_signal.connect(lambda new_text: ram_usage_bar.setValue(new_text[1]))
+    ram_usage_thread.start()
+
+    # show governor profile
+    try:
+        with open("/sys/devices/system/cpu/cpufreq/policy0/scaling_available_governors") as f:
+            text = f.readlines()[0].strip().split(" ")
+    except:
+        print_on_label("Some error to read governos\n")
+        text = ['error']
+
+    cpu_governor = QComboBox()
+    cpu_governor.setMinimumSize(int(group_size.width() / 2) - 2, 30)
+    layout_left.addWidget(cpu_governor)
+    cpu_current_gov = set_current_gov_thread()
+    cpu_current_gov.update_label_signal.connect(lambda new_text: cpu_governor.setCurrentText(new_text))
+    cpu_current_gov.start()
+    cpu_governor.addItems(text)
+    cpu_governor.currentTextChanged.connect(change_governor)
 
     window.show()
     sys.exit(app.exec())
